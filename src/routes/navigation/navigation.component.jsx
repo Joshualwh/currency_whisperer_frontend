0b1e4f62bca9a8b1d011e8a6f8dc9f63ae9b12cf
Joshualwh <joshualing0711@gmail.com>
2024-11-20 23:14:19 +0800

    Started with Navigation.

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+
+const Navigation = () => {
+  return (
+    <div>
+      <h1>Navigation</h1>
+      <h2>All Currency</h2>
+      <h2>Subscribed Currency</h2>
+      <h2>Money Changer Near Me</h2>
+    </div>
+  )
+}
+
+export default Navigation;
