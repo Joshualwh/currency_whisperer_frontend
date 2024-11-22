02fe99c6d9feab3d61e860113f089c5798611565
Joshualwh <joshualing0711@gmail.com>
2024-11-22 18:18:00 +0800

    Added navigation css and logo.

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,30 +1,49 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import { Outlet, Link } from "react-router-dom";
 
+import { ReactComponent as CWLogo} from "../../assets/currency-whisperer-high-resolution-logo-transparent.svg";
+
+import "./navigation.styles.scss";
+
 const Navigation = () => {
+
+  const [isDropdownVisible, setDropdownVisible] = useState(false);
+
+  const handleMouseEnter = () => {
+    setDropdownVisible(true);
+  };
+
+  const handleMouseLeave = () => {
+    setDropdownVisible(false);
+  };
+
   return (
     <Fragment>
-      <div>
-        <div>
-          <Link to='/'>
-            <h1>Currency Whisperer</h1>
-          </Link>
-          <label>
-            Subscription
-            <select name="" id="">
-              <option value="">All</option>
-              <option value="">Subscribed</option>
-            </select>
-          </label>
-          <h2>Money Changer</h2>
+      <span className="nav-bar-container">
+        <div className="nav-bar-left">
+          <Link className="logo-container" to='/'><CWLogo className="logo"/></Link>
+          <div className="nav-component" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+            <h3>Currency Conversion</h3>
+            {isDropdownVisible && (
+              <div>
+                <ul>
+                  <li>All</li>
+                  <li>Subscribed</li>
+                </ul>
+              </div>
+            )}
+          </div>
+          <div className="nav-component"><h3>Money Changer</h3></div>
         </div>
-        <div>
-          <h2>User Profile</h2>
+        <div className="nav-bar-right">
+          <div className="nav-component"><h3>User Profile</h3></div>
         </div>
-      </div>
+      </span>
       <Outlet />
     </Fragment>
   )
 }
 
 export default Navigation;
+
+// https://medium.com/how-to-react/how-to-create-a-dropdown-menu-on-hover-in-react-js-without-any-package-b16b2f76db71÷
